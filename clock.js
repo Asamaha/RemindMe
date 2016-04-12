a32e3a48fdad7654f22e0f8d123f34ce8717a702
Adam Samaha <adamsamahaa@gmail.com>
2016-04-11 17:18:32 -0700

    Gets config options for kue based on local/dev/prod Redis instance

--- clock.js
+++ clock.js
@@ -4,3 +4,6 @@ var redis = require('kue/lib/redis');
 // Senders
 var sendEmail = require('./senders/email');
 var sendSms = require('./senders/sms')
+
+// Gets config options for kue based on local/dev/prod Redis instance
+var kueOptions = require('./server/kueConfig.js');
